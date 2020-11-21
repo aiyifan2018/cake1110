f4f0d774a5c9cd66edf0c3263f580dcbf3cb79da
lxk <2411127573@qq.com>
2020-11-21 09:02:17 +0800

    'zml'

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -2,59 +2,62 @@
   <div>   
     <div class="backtop">
       <router-link to="/" class="routerLink">
-        <img src="../../public/img/index_enabled.png" alt="">
+        <img src="../../public/img/icons/setup.png" alt="">
       </router-link><br>
-      <mt-button type="danger" size="small" class="relogin">登录/注册</mt-button>
+      <mt-button type="danger"   class="relogin">登录/注册</mt-button>
     </div>
-    
-      <div class="orders">
+    <div class="ordermain">
+      <div class="orders" >
         我的订单
         <router-link to="/" class="order">
           全部订单>
         </router-link>
+        <hr class="hrs" />
       </div>
       <ul class="payment">
-        <li><img src="../../public/img/index_enabled.png" slot="icon">
+        <li><img src="../../public/img/icons/paid.png" slot="icon">
           <p>待付款</p>  
         </li>
         <li>
-          <img src="../../public/img/index_enabled.png" slot="icon">
+          <img src="../../public/img/icons/delivered.png" slot="icon">
           <p>今日配送</p>         
         </li>
         <li>
-          <img src="../../public/img/index_enabled.png" slot="icon">
+          <img src="../../public/img/icons/evaluated.png" slot="icon">
           <p>待评价</p>  
         </li>
       </ul>
+    </div>
+      
   
     <ul class="about">
       <li>
         <router-link to="/">
-          <img src="../../public/img/index_enabled.png">
+          <img src="../../public/img/icons/coupon.png">
           优惠劵
         </router-link>
       </li>
       <li>
         <router-link to="/">
-          <img src="../../public/img/index_enabled.png">
+          <img src="../../public/img/icons/address.png">
           收货地址
         </router-link>
       </li>
        <li>
         <router-link to="/">
-          <img src="../../public/img/index_enabled.png">
+          <img src="../../public/img/icons/customer.png">
           联系客服
         </router-link>
       </li>
       <li>
         <router-link to="/">
-          <img src="../../public/img/index_enabled.png">
+          <img src="../../public/img/icons/problem.png">
           常见问题
         </router-link>
       </li>
       <li>
         <router-link to="/">
-          <img src="../../public/img/index_enabled.png">
+          <img src="../../public/img/icons/about.png">
           关于我们
         </router-link>
       </li>
@@ -62,8 +65,25 @@
   </div>
 </template>
 <style scoped>
+a{
+  color: #000 !important;
+}
+.hrs{
+  
+ border: 0.0.5px solid #000;
+  margin-top: 20px;
+}
   .backtop{
     background: url(../../public/img/background.svg);
+    height: 125px;
+  }
+  .ordermain{
+    background-color:#fff;
+    width: 95%;
+     margin: auto;
+    margin-top: -20px;
+   box-shadow: 0px 0px 10px #ccc;
+     
   }
   .routerLink{
     margin-top: 10px;
@@ -72,15 +92,23 @@
   }
   .relogin{
     display: block;
-    margin: 50px auto;
+    margin: 30px auto;
     text-align: center;
+    padding: 5px 20px;
   }
   .about{
     margin-top: 20px;
   }
+  .about>li{
+    padding: 10px 20px;
+    color: #000 !important;
+  }
   .orders{
-    border-bottom: 1px solid #000;
+    /* border-bottom: 1px solid #666; */
     padding: 10px 15px;
+     
+    
+     
   }
   
   .order{
@@ -93,13 +121,18 @@
     list-style: none;
     
   }
+  .payment{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 100px;
+    margin-top: -20px;
+  }
   .payment>li{
     text-align: center;
     margin-left: 100px;
   }
-  .payment{
-    display: flex;
-  }
+  
   .payment>li{
     float: left;
     margin: 0 25px;
