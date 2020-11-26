9455bb959112d8b1445868354b7a57069d14967c
308219576 <71804010+308219576@users.noreply.github.com>
2020-11-26 13:00:14 +0800

    Add files via upload

---
+++ pay.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <mt-header title="填写订单">
+        <router-link slot="left" to="/shoppingcart">
+            <mt-button  icon="back"></mt-button>
+        </router-link>
+    </mt-header>
+    <div class="top">
+          <div class="top-name">姓名：杜兰特</div>
+          <div class="top-tel">电话：13912365478</div>
+          <div class="top-address">地址：美国纽约州布鲁克林区</div>
+    </div>
+    <div>
+        <mt-field label="送达日期" placeholder="请输入" type="date" v-model="day">
+            <mt-picker :slots="slots" @change="onValuesChange"></mt-picker>
+        </mt-field>
+        <mt-field label="订购人姓名" placeholder="请输入姓名" v-model="username"></mt-field>
+        <mt-field label="留言" placeholder="选填" type="textarea" rows="4" v-model="introduction"></mt-field>
+    </div>
+    <div class="bottom-bar">
+        <!-- 选项卡开始 -->
+        <!--id仅在此父元素组件内唯一即可，整个页面内id可不唯一，表示对应变量,vue中特有-->
+        <div class="heji" >合计：¥{{this.$route.params.price}}</div>
+        <div class="jiesuan" ><router-link to="ok">去结算</router-link></div>       
+    </div>
+  </div>
+
+</template>
+<script>
+export default {
+    methods: {
+      onValuesChange(picker, values) {
+        if (values[0] > values[1]) {
+          picker.setSlotValue(1, values[0]);
+        }
+      }
+    },
+    data() {
+      return {
+        day:"请选择",
+        username:"Kevin Durant",
+        introduction:"",
+        slots: [
+          {
+            flex: 1,
+            values: ['2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07', '2020-08', '2020-09', '2020-10', '2020-11', '2020-12'],
+            className: 'slot1',
+            textAlign: 'right'
+          }, {
+            divider: true,
+            content: '-',
+            className: 'slot2'
+          }, {
+            flex: 1,
+            values: ['2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07', '2020-08', '2020-09', '2020-10', '2020-11', '2020-12'],
+            className: 'slot3',
+            textAlign: 'left'
+          }
+        ]
+      };
+    }
+  }
+</script>
+<style scoped>
+.href{
+  text-decoration: none;
+}
+  .top div{
+    /* padding-top: -55px; */
+    margin-bottom: 0px;
+    margin-left: 10px;
+    align-items: center;
+    text-align: left;
+    margin-top: 15px;
+  }
+   .bottom-bar{
+    text-align: center;
+    /* display:  */
+    display: flex;
+    position: relative;
+    bottom: -89px;
+  }
+  .heji{
+    background: #e8e8e8;
+    /* display: block; */
+    font-size: 25px;
+    width: 66%;
+  }
+  .jiesuan{
+    /* display: block; */
+    font-size: 25px;
+    background-color: lightblue;
+    width: 34%;
+  }
+</style>
